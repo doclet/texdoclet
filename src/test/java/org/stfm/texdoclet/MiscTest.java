4839710bca9b3b8b8500556afd2f5c688f52e5aa
stfm <stfm@doclet>
2015-03-11 01:28:20 +0100

    added another test class

---
+++ src/test/java/org/stfm/texdoclet/MiscTest.java
@@ -0,0 +1,27 @@
+package org.stfm.texdoclet;
+
+/**
+ * This class is for testing Java syntax not present in implementation code.
+ *
+ * @author Stefan Marx
+ */
+public class MiscTest {
+
+	/** An array of objects */
+	public Object[] objects;
+
+	/**
+	 * @return objects
+	 */
+	public Object[] getObject() {
+		return this.objects;
+	}
+
+	/**
+	 * @param object the object
+	 */
+	public void setObject(final Object[] objects) {
+		this.objects = objects;
+	}
+
+}
